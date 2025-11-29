865f7d4be33a723ad63692b2951d4c1ec4a01cc4
melonhong <melonhong46@gmail.com>
2025-11-29 15:47:56 +0900

    feat: FitnessAverage 클래스 추가
    - 평균값의 데이터를 담는 내부 클래스
    - 프론트에서 사용자와 평균값을 비교하기 위해 추가함

--- src/main/java/com/fitlink/web/dto/FitnessResponseDTO.java
+++ src/main/java/com/fitlink/web/dto/FitnessResponseDTO.java
@@ -1,10 +1,7 @@
 package com.fitlink.web.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -29,4 +26,36 @@ public class FitnessResponseDTO {
 
     @Schema(description = "순발력(Quickness) - 제자리 멀리뛰기(standingLongJump) 또는 슬라이더 점수로부터 계산됨")
     private Float quickness;
+
+    @Schema(description = "운동 종목별 대한민국 평균(기준) 데이터 객체")
+    private FitnessAverage average;
+
+    /**
+     * 평균값 데이터를 담는 내부 클래스
+     */
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    @Builder
+    public static class FitnessAverage {
+
+        @Schema(description = "근력(Strength) 평균 - 악력 측정값 기준")
+        private Float gripStrength;
+
+        @Schema(description = "근지구력(Muscular Endurance) 평균 - 윗몸일으키기 측정값 기준")
+        private Float sitUp;
+
+        @Schema(description = "유연성(Flexibility) 평균 - 앉아 윗몸 앞으로 굽히기 측정값 기준")
+        private Float sitAndReach;
+
+        @Schema(description = "심폐지구력(Cardiopulmonary Endurance) 평균 - 20m 왕복 오래달리기 측정값 기준")
+        private Float shuttleRun;
+
+        @Schema(description = "민첩성(Agility) 평균 - 10m 왕복달리기 측정값 기준")
+        private Float sprint;
+
+        @Schema(description = "순발력(Quickness) 평균 - 제자리 멀리뛰기 측정값 기준")
+        private Float standingLongJump;
+    }
 }
