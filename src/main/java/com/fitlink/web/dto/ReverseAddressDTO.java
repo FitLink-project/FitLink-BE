3a79d7120fb7ea7d92311adff84baaef2b3e5574
wonee1 <gimchaewon417@gmail.com>
2025-12-06 03:25:44 +0900

    feat : 사용자 위치로 주소 반환

---
+++ src/main/java/com/fitlink/web/dto/ReverseAddressDTO.java
@@ -0,0 +1,10 @@
+package com.fitlink.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ReverseAddressDTO {
+    private String fullAddress;
+}
